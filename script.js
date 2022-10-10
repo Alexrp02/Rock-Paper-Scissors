a3dd1f390dee2e7dea1babfb92737156670d7732
AlexRP <aleeexrp02@gmail.com>
2022-10-10 21:24:12 +0200

    Made function to get random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hey!')
+function getComputerChoice () {
+    let number = Math.floor(Math.random()*3);
+    switch (number) {
+        case 0 :
+            return("Rock");
+        case 1 :
+            return("Paper");
+        case 2 :
+            return("Scissors");
+    }
+}
+
+console.log(getComputerChoice());
