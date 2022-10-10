2666d36cd6d9cacb7450939b7d1fd00234e7e659
AlexRP <aleeexrp02@gmail.com>
2022-10-10 21:38:10 +0200

    Round function finished and working properly.

--- script.js
+++ script.js
@@ -10,4 +10,33 @@ function getComputerChoice () {
     }
 }
 
-console.log(getComputerChoice());
+function round (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase()==computerSelection.toLowerCase()){
+        return('Draw!');
+    }else if(playerSelection.toLowerCase()=="rock"){
+        if (computerSelection.toLowerCase() == 'paper') {
+            return('You lost! Paper beats Rock')
+        }else{
+            return('You won! Rock beats Scissors')
+        }
+    }else if(playerSelection.toLowerCase()=="paper"){
+        if (computerSelection.toLowerCase() == 'scissors') {
+            return('You lost! Scissors wins Paper')
+        }else{
+            return('You won! Paper wins Rock')
+        }
+    }else {
+        if (computerSelection.toLowerCase() == 'rock') {
+            return('You lost! Rock beats Scissors')
+        }else{
+            return('You won! Scissors win Paper')
+        }
+    }
+}
+
+const playerSelection = 'Scissors' ;
+const machineSelection = getComputerChoice() ;
+
+console.log('Player says ' + playerSelection)
+console.log("Computer says: " + machineSelection);
+console.log(round(playerSelection, machineSelection)) ;
