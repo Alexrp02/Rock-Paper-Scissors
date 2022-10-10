8363bde8107b795f9b2fbf80d163925fee722438
AlexRP <aleeexrp02@gmail.com>
2022-10-10 21:55:12 +0200

    Game acting as wanted with prompts.

--- script.js
+++ script.js
@@ -10,33 +10,57 @@ function getComputerChoice () {
     }
 }
 
+let wins = 0;
+let machineWins = 0 ;
+
 function round (playerSelection, computerSelection) {
+    console.log("Computer says: " + computerSelection);
     if (playerSelection.toLowerCase()==computerSelection.toLowerCase()){
         return('Draw!');
     }else if(playerSelection.toLowerCase()=="rock"){
         if (computerSelection.toLowerCase() == 'paper') {
+            machineWins+=1;
             return('You lost! Paper beats Rock')
         }else{
+            wins+=1;
             return('You won! Rock beats Scissors')
         }
     }else if(playerSelection.toLowerCase()=="paper"){
         if (computerSelection.toLowerCase() == 'scissors') {
+            machineWins+=1;
             return('You lost! Scissors wins Paper')
         }else{
+            wins+=1;
             return('You won! Paper wins Rock')
         }
     }else {
         if (computerSelection.toLowerCase() == 'rock') {
+            machineWins+=1;
             return('You lost! Rock beats Scissors')
         }else{
+            wins+=1;
             return('You won! Scissors win Paper')
         }
     }
 }
 
-const playerSelection = 'Scissors' ;
-const machineSelection = getComputerChoice() ;
+function game() {
+    const rounds = parseInt(prompt('Enter how many rounds do you want to play: ')) ;
+    let playerSelection;
+    for(let i=0 ; i<rounds ; i++) {
+        playerSelection = prompt('Enter paper, rock or scissors: ')
+        alert(round(playerSelection, getComputerChoice()))
+    }
 
-console.log('Player says ' + playerSelection)
-console.log("Computer says: " + machineSelection);
-console.log(round(playerSelection, machineSelection)) ;
+    alert('Machine won ' + machineWins + ' and you ' + wins) ;
+
+    if (wins > machineWins){
+        alert('You won the game!!');
+    }else if (wins < machineWins){
+        alert('You lost again the machine!!');
+    }else {
+        alert('You and the machine draw!!');
+    }
+}
+
+game() ;
